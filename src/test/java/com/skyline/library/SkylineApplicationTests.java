e390b8c476766780fd2e6c620e0543c914db9a09
SandalikaSC <66319075+SandalikaSC@users.noreply.github.com>
2023-09-21 13:12:30 +0530

    Initial commit

---
+++ src/test/java/com/skyline/library/SkylineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skyline.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkylineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
